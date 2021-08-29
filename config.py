a1782d7068e777e520125dbc65dee0caa52b6ebc
Akshett Rai Jindal <akshett.jindal@research.iiit.ac.in>
2021-08-30 01:10:15 +0530

    add comments in config.py

--- config.py
+++ config.py
@@ -12,11 +12,22 @@ class Config:
             "db_name": ""
         }
 
+        # The emails and the user ids will be forwarded here
         self.log_channel_id = -100
+
+        # The group where the bot will forward the messages and can be controlled
         self.control_group_id = -100
+
+        # The ID of the main group of which the person should be a member to be able to send messages through the bot
         self.group_neccessary = -100
+
+        # The link to the google group
         self.google_group_link = "https://groups.google.com/g/<GroupName>"
+
+        # You can point the members here to get invites to the group
         self.invite_channel_link = "@ChannelUsername"
+
+        # Group for queries and help
         self.help_group_link = "t.me/ChannelUsername"
 
         # GIFs : List[String]
